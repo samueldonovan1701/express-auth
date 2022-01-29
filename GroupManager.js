a2cd8bf7f40a3c280ef2d4e50c968b2ed846dbcd
Samuel Donovan <samuel.donovan1701@gmail.com>
2022-01-29 13:52:12 -0700

    Add models to groups

--- GroupManager.js
+++ GroupManager.js
@@ -1,31 +1,59 @@
 'use strict';
 
 class GroupManager {
-	constructor() {
+	constructor(model = {}) {
 		this._groups = {};
+		this._model = model;
 	}
 
-//Methods
-	new(id, users=[]) {
+//Create
+	new(id, users=[], model=this._model) {
 		if(id in this._groups)
 			return false;
 
-		this._groups[id] = users;
+		this._groups[id] = {
+			"users": users,
+			"model": model
+		};
 		return true;
 	}
-	del(id) {
-		if(!(id in this._groups))
-			return false;
 
-		delete this._groups[id];
-		return true;
+//Read
+	get(id) {
+		let group = this._groups[id];
+
+		if(group == undefined)
+			return undefined;
+		else
+			return group.model;
+	}
+	getUsers(id) {
+		let group = this._groups[id];
+
+		if(group == undefined)
+			return undefined;
+		else
+			return group.users;
 	}
 	all() {
-		return this._groups.keys();
+		return Object.keys(this._groups);
 	}
 
+//Update
+	set(id, model) {
+		let group = this._groups[id];
+
+		if(_groups == undefined)
+			return undefined;
+		else {
+			group.model = model;
+			return true;
+		}
+	}
 	setID(old, updated) {
-		if(updated in this._groups || !(old in this._groups))
+		if(!(old in this._groups))
+			return undefined;
+		if(updated in this._groups)
 			return false;
 
 		this._groups[updated] = this._groups[old];
@@ -33,32 +61,44 @@ class GroupManager {
 		return true;
 	}
 	setUsers(id, users) {
-		if(!(id in this._groups))
-			return false;
+		let group = this._groups[id];
 
-		this._groups[id] = users;
-		return true;
-	}
-	getUsers(id) {
-		if(!(id in this._groups))
-			return false;
-
-		return this._groups[id];
+		if(group == undefined)
+			return undefined;
+		else {
+			group.users = users;
+			return true;
+		}
 	}
 	addUsers(id, users) {
-		if(!(id in this._groups))
-			return false;
+		let group = this._groups[id];
 
-		this._groups[id].concat(users);
-		return true;
+		if(group == undefined)
+			return undefined;
+		else {
+			group.users.concat(users);
+			return true;
+		}
 	}
 	remUsers(id, users) {
-		if(!(id in this._groups))
-			return false;
+		let group = this._groups[id];
 
-		this._groups[id].filter((groupmemeber) => { 
-			return users.indexOf(groupmemeber) == -1; 
-		});
+		if(group == undefined)
+			return undefined;
+		else {
+			group.users.filter((groupmemeber) => { 
+				return users.indexOf(groupmemeber) == -1; 
+			});
+			return true;
+		}
+	}
+
+//Delete
+	del(id) {
+		if(!(id in this._groups))
+			return undefined;
+
+		delete this._groups[id];
 		return true;
 	}
 };
