3167c63f7fb14c0c6ad66b95fa3b797342731c05
Samuel Donovan <samuel.donovan1701@gmail.com>
2022-01-29 14:01:53 -0700

    Fix typos and remove variadic functionality
    Variadic function syntax was confusing

--- GroupManager.js
+++ GroupManager.js
@@ -7,7 +7,7 @@ class GroupManager {
 	}
 
 //Create
-	new(id, model=this._model, ...users) {
+	new(id, model=this._model, users) {
 		if(id in this._groups)
 			return false;
 
@@ -43,7 +43,7 @@ class GroupManager {
 	set(id, model) {
 		let group = this._groups[id];
 
-		if(_groups == undefined)
+		if(group == undefined)
 			return undefined;
 		else {
 			group.model = model;
@@ -70,7 +70,7 @@ class GroupManager {
 			return true;
 		}
 	}
-	addUsers(id, ...users) {
+	addUsers(id, users) {
 		let group = this._groups[id];
 
 		if(group == undefined)
@@ -80,7 +80,7 @@ class GroupManager {
 			return true;
 		}
 	}
-	remUsers(id, ...users) {
+	remUsers(id, users) {
 		let group = this._groups[id];
 
 		if(group == undefined)
