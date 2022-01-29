cf28379891755065d346c5f96e644e91447867f3
Samuel Donovan <samuel.donovan1701@gmail.com>
2022-01-29 14:34:24 -0700

    Add periodic purging of expired tokens

--- TokenManager.js
+++ TokenManager.js
@@ -3,44 +3,66 @@
 var crypto = require('crypto');
 
 class TokenManager {
-	constructor(model = {}) {
+	constructor(model = {}, maxAge=3600000, purgeInterval=3600000) {
 		this._tokens = {};
 		this._model = model;
 
-		this.purgeInterval = 3600;
+		this.purgeInterval = purgeInterval;
+		this._periodicPurge();	
 
-		this.maxAge = 3600;
+		this.maxAge = maxAge;
 		this.httpOnly = true;
 		this.sameSite = true;
 		this.secure = true;
 	}
 
 //Create
-	new(userId, model=this._model) {
-		if(id in this._tokens)
-			return false;
-
+	new(userId, model=this._model, maxAge=this.maxAge) {
 		let tokenId = crypto.createHash('md5')
 			.update(userId)
-			.update(Date.now().toString());
+			.update(Date.now().toString())
 			.digest('hex');
 
 		this._tokens[tokenId] = { 
 			user: userId,
-			expires: Date.now() + this.maxAge*1000,
+			expires: Date.now() + maxAge,
 			model: model
 		};
 		return tokenId;
 	}
 
 //Read
-	get(id) {		
+	get(id) {
 		let token = this._tokens[id];
 
 		if(token == undefined)
 			return undefined;
 		else
 			return token;
+	}
+	getExpires(id) {
+		let token = this._tokens[id];
+
+		if(token == undefined)
+			return undefined;
+		else
+			return token.expires;
+	}
+	getUser(id) {
+		let token = this._tokens[id];
+
+		if(token == undefined)
+			return undefined;
+		else
+			return token.user;
+	}
+	getModel(id) {
+		let token = this._tokens[id];
+
+		if(token == undefined)
+			return undefined;
+		else
+			return token.model;
 	}
 	all() {
 		return Object.keys(this._tokens);
@@ -75,6 +97,17 @@ class TokenManager {
 
 		delete this._tokens[id];
 		return true;
+	}
+	purge() {
+		const now = Date.now();
+		for (const [id, token] of Object.entries(this._tokens)) {
+			if(token.expires < now)
+				delete this._tokens[id];
+		}
+	}
+	_periodicPurge() {
+		this.purge();
+		setTimeout(this._periodicPurge.bind(this), this.purgeInterval);
 	}
 };
 
