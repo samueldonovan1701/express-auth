586befbd4946e76223fbfa98799c8a78d07667f0
Samuel Donovan <samuel.donovan1701@gmail.com>
2022-01-29 16:35:32 -0700

    Create AuthManager

---
+++ AuthManager.js
@@ -0,0 +1,74 @@
+'use strict';
+/* Injected Dependencies:
+
+userManager:
+	{bool} verifyPW(userID, pw)
+
+groupManager:
+	.
+
+tokenManager:
+	{int} maxAge
+	{bool} httpOnly
+	{bool} sameSite
+	{bool} secure
+
+	{string} new(userId, maxAge)
+	{bool} del(tokenId)
+	{string} getUser(tokenId)
+*/
+
+
+class AuthManager {
+	 constructor(userManager, groupManager, tokenManager) {
+	 	//Managers
+	 		this.users = userManager;
+	 		this.groups = groupManager;
+	 		this.tokens = tokenManager;
+	 }
+
+//Methods
+	login(req, res, userID, pw) {
+		if(req.signedCookies['session'] != undefined)
+			return 409; //Cookie exists; Already logged in
+
+		let login = this.users.verifyPW(userID, pw);
+		if(!login)
+			return login; //Login failed
+
+		let tokenId = this.tokens.new(userId);
+		if(!tokenId)
+			return 500;
+
+		res.signedCookie('session', token, {
+			expires: new Date(Date.now() + this.tokens.maxAge*1000),
+			maxAge: this.tokens.maxAge, //In seconds
+			httpOnly: this.tokens.httpOnly,
+			sameSite: this.tokens.sameSite,
+			secure: this.tokens.secure
+		});
+	}
+
+	logout(req, res) {
+		let tokenId = req.signedCookies['session'];
+
+		if(tokenId == undefined)
+			return 409; //Cookie dne; Not logged in
+
+		let logout = this.tokens.del(token);
+		if(!logout)
+			return logout;
+
+		res.clearCookie('session');
+	}
+
+	isLoggedIn(req, res) {
+		return this.getCurrentUser(req, res) != false;
+	}
+
+	getCurrentUser(req, res) {
+		return this.tokens.getUser(req.signedCookies['session']);
+	}
+};
+
+module.exports = AuthManager;
