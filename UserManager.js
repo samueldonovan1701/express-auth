4cd3e900cee6f3fd3ca26ffe54fa20ae2852b4e8
Samuel S. Donovan <samuel.donovan1701@gmail.com>
2022-01-29 14:36:56 -0700

    User manager (#2)
    * Rename file
    
    * UserManager class
    
    * Change UserManager to DefaultUserManager
    
    * Change module export
    
    * Revert back to UserManager
    
    * Add setID
    
    * Add models (profiles) to users
    
    * Change operational orders for optimization
    
    * Fix constructor ignoring provided model

---
+++ UserManager.js
@@ -0,0 +1,84 @@
+'use strict';
+
+class UserManager {
+	constructor(model = {}) {
+		this._users = {};
+		this._model = model;
+	}
+
+//Create
+	new(id, pw="", model=this._model) {
+		if(id in this._users)
+			return false;
+
+		this._users[id] = {
+			"pw": pw,
+			"model": model
+		};
+		return true;
+	}
+
+//Read
+	get(id) {
+		let user = this._users[id];
+
+		if(user == undefined)
+			return undefined;
+		else
+			return user.model;
+	}
+	verifyPW(id, pw) {
+		let user = this._users[id];
+
+		if(user == undefined)
+			return undefined;
+		else
+			return user.pw == pw;
+	}
+	all() {
+		return Object.keys(this._users);
+	}
+
+//Update
+	set(id, model) {
+		let user = this._users[id];
+
+		if(user == undefined)
+			return undefined;
+		else {
+			user.model = model;
+			return true;
+		}
+	}
+	setID(old, updated) {
+		if(!(old in this._users))
+			return undefined;
+		if(updated in this._users)
+			return false;
+
+		this._users[updated] = this._users[old];
+		delete this._users[old];
+		return true;
+	}
+	setPW(id, newPW) {
+		let user = this._users[id];
+
+		if(user == undefined)
+			return undefined;
+		else {
+			user.pw = newPW;
+			return true;
+		}
+	}
+
+//Delete
+	del(id) {
+		if(!(id in this._users))
+			return undefined;
+
+		delete this._users[id];
+		return true;
+	}
+};
+
+module.exports = UserManager;
