24a576a4ff493ad8e5af810f30efbd534df1e0a3
Samuel S. Donovan <samuel.donovan1701@gmail.com>
2022-12-10 19:50:37 -0700

    Update UserManager to employ input validation

--- UserManager.js
+++ UserManager.js
@@ -2,78 +2,117 @@
 
 class UserManager {
 	#users;
-	#model;
-	constructor(model = {"id":"", "password":""}) {
+	defaultModel;
+	constructor(defaultModel = {"id":"", "password":""}) {
+		// Input Validation
+		if(typeof defaultModel != "object")
+			throw TypeError("express-cookie-session-auth.UserManager(defaultModel): typeof `defaultModel` is not 'object'")
+
 		this.#users = {};
-		this.model = model;
+		this.defaultModel = defaultModel;
 	}
 
-//Create
-	add(id, password, model=this.model) {
+/*******************************************************************************
+********************************************************************************
+* Create Functions
+********************************************************************************
+*******************************************************************************/
+	add(id, password, model=this.defaultModel) {
+		// Input Validation
+		if(typeof id != "string")
+			throw TypeError("express-cookie-session-auth.UserManager.add(id, password, model): typeof `id` is not 'string'")
+		if(typeof password != "string")
+			throw TypeError("express-cookie-session-auth.UserManager.add(id, password, model): typeof `password` is not 'string'")
+		if(typeof model != "object")
+			throw TypeError("express-cookie-session-auth.UserManager.add(id, password, model): typeof `model` is not 'object'")
+	
+		// Check that user exists
 		if(id in this.#users)
 			return false;
 
-		//Check that model is an object
-		if(typeof model != "object" && typeof model != "function")
-			model = {"value": model};
-
-		//Set id
+		// Set id
 		model.id = id;
 
-		//Set password
+		// Set password
 		model.password = password;
 
-		//Set
+		// Add User
 		this.#users[id] = model;
 
 		return true;
 	}
 
-//Read
+/*******************************************************************************
+********************************************************************************
+* Read Functions
+********************************************************************************
+*******************************************************************************/
 	get(id) {
+		// Input Validation
+		if(typeof id != "string")
+			throw TypeError("express-cookie-session-auth.UserManager.get(id): typeof `id` is not 'string'")
+	
+		// Get User
 		return this.#users[id];
 	}
 	verify(id, password) {
-		let user = module.exports.users.get(id);
+		// Input Validation
+		if(typeof id != "string")
+			throw TypeError("express-cookie-session-auth.UserManager.verify(id, password): typeof `id` is not 'string'")
+		if(typeof password != "string")
+			throw TypeError("express-cookie-session-auth.UserManager.verify(id, password): typeof `password` is not 'string'")
+
+		// Get User
+		let user = this.#users[id];
 		if(user === undefined) //Username not found
 			return undefined;
 	
+		// Compare passwords
 		return (user.password === password);
 	}
 	all() {
 		return Object.keys(this.#users);
 	}
 
-//Update
-	set(id, model) {
-		let old = this.#users[id];
-
-		if(old === undefined)
+/*******************************************************************************
+********************************************************************************
+* Update Functions
+********************************************************************************
+*******************************************************************************/
+	set(id, model=this.defaultModel) {
+		// Input Validation
+		if(typeof id != "string")
+			throw TypeError("express-cookie-session-auth.UserManager.set(id, model): typeof `id` is not 'string'")
+		if(model.hasOwnProperty("password") && typeof model.password != "string")
+			throw TypeError("express-cookie-session-auth.UserManager.set(id, model): typeof `model.password` is not 'string'")
+		if(typeof model != "object")
+			throw TypeError("express-cookie-session-auth.UserManager.set(id, model): typeof `model` is not 'object'")
+		
+		// Check if user exists
+		let current = this.#users[id];
+		if(current === undefined)
 			return undefined;
-		else {
-			//Check that model is an object			
-			if(typeof model != "object" && typeof model != "function")
-				model = {"value": model};
 
-			//Check model.password exists and is a string. If not, keep old
-			if(!model.hasOwnProperty("password"))
-				model.password = old.password
-			if(typeof model.password != "string") 
-				model.password = old.password;
+		//Check model.password exists. If not, keep old
+		if(!model.hasOwnProperty("password"))
+			model.password = current.password
 
-			//Set id
-			model.id = id;
+		//Set id
+		model.id = id;
 
-			//Set model
-			this.#users[id] = model;
+		//Set model
+		this.#users[id] = model;
 
-			return true;
-		}
+		return true;
 	}
 
 //Delete
 	del(id) {
-		if(!(id in this.#users))
+		// Input Validation
+		if(typeof id != "string")
+			throw TypeError("express-cookie-session-auth.UserManager.del(id): typeof `id` is not 'string'")
+		
+		if(this.#users[id] == undefined)
 			return undefined;
 
 		delete this.#users[id];
